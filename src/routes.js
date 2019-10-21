698153624e96abbf33b3662e421f5046d9154188
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2019-10-20 23:45:02 -0300

    Routes configurated

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+const express =  require('express')
+const multer = require('multer')
+const uploadConfig = require('./config/upload')
+
+const SessionController = require('./Controllers/SessionController')
+const FoodController = require('./Controllers/FoodController')
+const MenuController = require('./Controllers/MenuController')
+
+const routes = express.Router()
+const upload = multer(uploadConfig)
+
+routes.post('/sessions', SessionController.store)
+
+routes.post('/foods', upload.single('thumbnail'), FoodController.store)
+
+routes.get('/:name', MenuController.show)
+
+module.exports = routes
