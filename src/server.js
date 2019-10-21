3957a50f7020c07791a78de4b234edc6ec8eb79c
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2019-10-20 23:45:29 -0300

    Index file coded

--- src/server.js
+++ src/server.js
@@ -1,7 +1,21 @@
-const express = require('express')
+require("dotenv").config()
+
+const express =  require('express')
+const cors = require('cors')
+const mongoose =  require('mongoose')
+const routes =  require('./routes')
+const path = require('path')
 
 const app = express()
 
-app.get('/', )
+mongoose.connect('mongodb+srv://oministack:oministack@oministack-f1kph.mongodb.net/foodsup?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
 
-app.listen()
+app.use(cors())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(express.json())
+app.use(routes)
+
+app.listen(process.env.PORT || 3333)
