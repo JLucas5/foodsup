15344753e442638a9994eed25a92e3d02a204b4e
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2019-10-24 15:54:01 -0300

    Mongo Schema updated

--- src/Models/Restaurant.js
+++ src/Models/Restaurant.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose')
 
 const RestaurantSchema = new mongoose.Schema({
-   name: String,
-   url: String,
-   email: String,
-   cellphone: String
+   name: {
+      type: String,
+      require : [true, 'Name required']
+   },
+   url: {
+      type: String,
+      unique: true,
+      require: [true, 'Url required']
+   },
+   email:{
+      type: String,
+      required: [true, 'Email required'],
+      unique: true
+   } ,
+   cellphone: {
+      type: String,
+      require: [true, 'Cellphone required']
+   },
+   password: {
+      type: String,
+      required: [true, 'Password required'],
+      select: false
+   }
 })
 
 module.exports = mongoose.model('Restaurant', RestaurantSchema)
